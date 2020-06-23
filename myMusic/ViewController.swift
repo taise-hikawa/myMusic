a3dc0fc1b580301ccf10c985dd334bd1a0324e89
taisei.hikawa <cmmkarubi@gmail.com>
2020-06-23 17:00:53 +0900

    Initial Commit

---
+++ myMusic/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  myMusic
+//
+//  Created by Sakurako Shimbori on 2020/06/23.
+//  Copyright © 2020 Taisei Hikawa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
